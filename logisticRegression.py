76c1add97c72efd31a378e038c22012cccc8126c
a6dulrauf <a6dulrauf@gmail.com>
2020-03-01 21:30:56 +0500

    someChangesMyLogisticRegression

--- logisticRegression.py
+++ logisticRegression.py
@@ -1,71 +1,71 @@
 import numpy as np
 import math
 
+
 class LogisticRegression(object):
     def __init__(self, data=[], handlers=[], learningRate=.01, iterations=2000, threshold=0.000005):
         self.data = np.array([[1] + x for x in data])
         self.handlers = handlers
         self.learningRate = learningRate
         if data == []:
-            self.weights = np.zeros(3) 
+            self.weights = np.zeros(3)
         else:
-            self.weights = np.zeros(self.data.shape[1]) 
+            self.weights = np.zeros(self.data.shape[1])
         self.iterations = iterations
         self.threshold = threshold
 
     def setData(self, data):
         if data != []:
             self.data = np.array([[1] + x for x in data])
-            self.weights = np.zeros(self.data.shape[1]-1) 
-    
+            self.weights = np.zeros(self.data.shape[1] - 1)
+
     def fireHandler(self, finished):
         for handler in self.handlers:
-            handler(self.weights,finished)
+            handler(self.weights, finished)
 
     def sigmoid(self, sum):
-        return (math.exp(sum))/(math.exp(sum) + 1)
+        return (math.exp(sum)) / (math.exp(sum) + 1)
 
     def solve(self):
 
         iterationCount = 0
         outputCol = self.data.shape[1] - 1
-        while(iterationCount < self.iterations):
+        while (iterationCount < self.iterations):
             iterationCount += 1
-            
+
             tempWeights = np.copy(self.weights)
-            
-            for col in range(self.data.shape[1]-1):
+
+            for col in range(self.data.shape[1] - 1):
                 sum = 0
                 for row in self.data:
                     sum += (row[outputCol] - self.sigmoid(self.weights.dot(row[:-1]))) * row[col]
                 tempWeights[col] = self.weights[col] + self.learningRate * sum
 
-            self.threshold = abs(np.sum(tempWeights) - np.sum(self.weights)) 
+            self.threshold = abs(np.sum(tempWeights) - np.sum(self.weights))
 
             self.weights = np.copy(tempWeights)
 
             self.fireHandler(False)
         self.fireHandler(True)
 
-
     def calculate_boundary(self, data):
 
-        slope = -(self.weights[0]/self.weights[2])/(self.weights[0]/self.weights[1])  
-        intercept = -self.weights[0]/self.weights[2]
+        slope = -(self.weights[0] / self.weights[2]) / (self.weights[0] / self.weights[1])
+        intercept = -self.weights[0] / self.weights[2]
         if math.isnan(slope):
             slope = 0.0
         if math.isnan(intercept):
             intercept = 0.0
-        y =  slope * data + intercept
+        y = slope * data + intercept
         # print("slope = %f, intercept = %f"%(slope,intercept))
         return y
-    
+
     def predict(self, data):
         result = []
         for row in data:
             nt = [1] + row.tolist()
 
-            if self.sigmoid(self.weights.dot(np.array(nt) ))>= 0.5:
+            if self.sigmoid(self.weights.dot(np.array(nt))) >= 0.5:
                 result.append(1)
             else:
                 result.append(-1)
@@ -73,13 +73,13 @@ class LogisticRegression(object):
 
 
 if __name__ == "__main__":
-    data = [[1,3,5],
-    [2,4,5],
-    [7,8,5]]
+    data = [[1, 3, 5],
+            [2, 4, 5],
+            [7, 8, 5]]
     lg = LogisticRegression()
     lg.setData(data)
-    #lg.solve()
+    # lg.solve()
     print(lg.data.shape)
-    l =[1,2,3,4,5]
+    l = [1, 2, 3, 4, 5]
     print(lg.data)
     print(data)
--- myLogisticResgression.py
+++ myLogisticResgression.py
@@ -1,25 +1,58 @@
+import math
+import numpy as np
 
 
-
-class LogisticRegression():
+class LogisticRegression(object):
 
     def __init__(self):
         pass
 
+    def setData(self, data):
+        if data != []:
+            self.data = np.array([[np.random.randint(low=0, high=2)] + x for x in data])
+            self.weights = np.ones(self.data.shape[1] - 1)
+            self.m = self.data.shape[0]
+
     def solve(self):
         pass
 
-    #w0+w1*x1+...
+    # w0+w1*x1+...
     def equation(self, x):
-        pass
+        h = self.weights.dot(x)
+        print(h)
 
     # 1/1+e-equation
-    def segmoid(self, equation):
-        pass
+    def sigmoid(self, equation):
+        return 1 / (math.exp(-equation) + 1)
 
     def cal_error(self):
         pass
 
-    #- 1/m [ sum i=1 to m y(i) * log (equation(i)) + (1-y(i)) * log(1-(equation(i)))]
+    # - 1/m [ sum i=1 to m y(i) * log (equation(i)) + (1-y(i)) * log(1-(equation(i)))]
     def cost(self):
-        pass
+        cost_list = []
+        for i in range(self.m):
+            cost = -1/self.m * (sum(self.data[i][0] * math.log(self.equation(self.data[:, 1:][i]))
+                                 + ((1-self.data[i][0]) * math.log(1-self.equation(self.data[:,1:][i])))))
+            print(cost)
+
+if __name__ == '__main__':
+    data = [[1, 3, 5],
+            [2, 4, 5],
+            [7, 8, 5]]
+    logistic = LogisticRegression()
+    logistic.setData(data)
+
+    print(logistic.data[:, 1:])
+    print(logistic.data[:, 1:][1])
+    '''
+    print(logistic.data[:, 0])
+    print(logistic.data.shape[0])
+    print(logistic.weights)
+    
+    print(logistic.data[0][0])
+    print(logistic.data[1][0])
+    print(logistic.data[2][0])
+    '''
+    #logistic.equation(logistic.data[:, 1:][0])
+    print(logistic.cost())
